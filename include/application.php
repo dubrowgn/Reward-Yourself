b61b7893b1e6adaf4f5a250d6338a356eee3f996
Dustin Brown <dubrowgn@gmail.com>
2014-03-27 10:11:56 +1000

    Update application.php
    Added comments for Request class

--- include/application.php
+++ include/application.php
@@ -29,21 +29,46 @@ function twig() {
 	]);
 } // twig( )
 
+/**
+ * A class for easily accessing GET/POST variables and related information
+ * @package default
+ */
 class Request {
+	/**
+	 * Retrieves the specified GET variable, or $default if it is not set
+	 * @param string $key The key of the variable to retrieve
+	 * @param mixed $default The default value to return if the variable is not set
+	 * @return mixed The specified GET variable or $default
+	 */
 	public function get($key, $default = null) {
 		return isset($_GET[$key]) ? $_GET[$key] : $default;
 	} // get( )
 
+	/**
+	 * Returns true if the request method was GET
+	 * @return bool true if the request method was GET
+	 */
 	public function isGet() {
 		return $_SERVER['REQUEST_METHOD'] === 'GET';
 	} // isGet( )
 
+	/**
+	 * Returns true if the request method was POST
+	 * @return bool true if the request method was POST
+	 */
 	public function isPost() {
 		return $_SERVER['REQUEST_METHOD'] === 'POST';
 	} // isPost( )
 
+	/**
+	 * Retrieves the specified POST variable, or $default if it is not set
+	 * @param string $key The key of the variable to retrieve
+	 * @param mixed $default The default value to return if the variable is not set
+	 * @return mixed The specified POST variable or $default
+	 */
 	public function post($key, $default = null) {
 		return isset($_POST[$key]) ? $_POST[$key] : $default;
 	} // post( )
 } // class
+
 ?>
